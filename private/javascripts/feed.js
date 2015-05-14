20af4e33bae752fa0a6c7597b1fad4ea8bf8f53b
Aleksandar Jankovic <aleksonline@icloud.com>
2015-05-14 14:28:10 +0200

    Added scanlines and more text.

--- private/javascripts/feed.js
+++ private/javascripts/feed.js
@@ -2,8 +2,11 @@
 
 var main = document.getElementById('main');
 var mainFeed = document.getElementById('mainFeed');
+var lastTimeout;
+var timeout = 50;
 var timeouts = new Array();
-var orgLogo = [
+//TODO Move to database
+var bootText = [
 '                           ####',
 '                 ####    #########    ####',
 '                ###########################',
@@ -32,34 +35,66 @@ var orgLogo = [
 '               ############################',
 '               #######  ##########  #######',
 '                 ###       ####       ###',
-'                           ####'
+'                           ####',
+'Initiating connection to HQ',
+'Syncing data',
+'Welcome, esteemed employee #166584',
+'WEATHER WARNING: High amount of acid rain predicted',
+'RECOMMENDATION: Stay indoors.',
+'Have a productive day!',
+'WEATHER WARNING: High amount of acid rain predicted. For real. WEATHER WARNING: High amount of acid rain predicted. For real',
 ];
-
-for(var i = 0; i < orgLogo.length; i++) {
-	setTimeout(addRow, 50 * i, orgLogo[i], true);
+var words = {
+	'connected' : 'Connected',
+	'done' : 'Done',
+	'loadingMarker' : '.'
 }
+var interruptionSound = new Audio('sounds/interruption.mp3');
+// interruptionSound.play();
 
 main.addEventListener('click', function() {
 	addRow();
 });
 
-function addRow(text, keepWhiteSpace) {
-	var elem = document.createElement('li');
+printBootText();
+
+function printBootText() {
+	for(var i = 0; i < bootText.length; i++) {
+		if (i > 28) {	
+			setTimeout(addRow, timeout * i, bootText[i], false);
+		} else {
+			setTimeout(addRow, timeout * i, bootText[i], true, 'logo');
+		}
+		console.log(timeout * i);
+	}
+}
+
+function addRow(text, keepWhiteSpace, extraClass) {
+	var row = document.createElement('li');
+	var span = document.createElement('span');
 	var text = document.createTextNode(text);
 
 	if(keepWhiteSpace) {
 		var pre = document.createElement('pre');
-		pre.appendChild(text);
-		elem.appendChild(pre);
-
+		// Add pre element around span to keep whitespace intact
+		pre.appendChild(span);
+		// Add pre element to li element
+		row.appendChild(pre);
 	} else {
-		elem.appendChild(text);
+		row.appendChild(span);
 	}
 
-	mainFeed.appendChild(elem);
-	setTimeout(scrollToBottom, 50);
+	if(extraClass) {
+		row.classList.add(extraClass);
+	}
+
+	// Add text to span element
+	span.appendChild(text);
+	// Add li element to ul element
+	mainFeed.appendChild(row);
+	setTimeout(scrollView, timeout, row);
 }
 
-function scrollToBottom() {
-	document.body.scrollTop = document.body.scrollHeight;
+function scrollView(element) {
+	element.scrollIntoView();
 }
