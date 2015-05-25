06d3350c9fb2b3406ede4dce4bd5e3f68807c320
Aleksandar Jankovic <aleksonline@icloud.com>
2015-05-25 15:17:18 +0200

    Tabs converted to spaces

--- routes/index.js
+++ routes/index.js
@@ -2,21 +2,21 @@ var express = require('express');
 var router = express.Router();
 
 function handle(io) {
-	router.get('/', function(req, res) {
-		res.render('index', { title: 'Organica Oracle v3.2' });
-	});
+    router.get('/', function(req, res) {
+        res.render('index', { title: 'Organica Oracle v3.2' });
+    });
 
-	io.on('connection', function(socket) {
-		console.log('a user connected');
+    io.on('connection', function(socket) {
+        console.log('a user connected');
 
-		socket.on('chat message', function(msg) {
-			socket.broadcast.emit('chat message', msg);
-		});
+        socket.on('chat message', function(msg) {
+            socket.broadcast.emit('chat message', msg);
+        });
 
-		socket.on('disconnect', function() {
-			socket.emit('chat message', 'User disconnected');
-		});
-	});
+        socket.on('disconnect', function() {
+            socket.emit('chat message', 'User disconnected');
+        });
+    });
 
     return router;
 }
