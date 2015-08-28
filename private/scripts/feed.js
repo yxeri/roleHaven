d8d1dfc4a355575ce2ac5553d3dd7f16dff44af3
Aleksandar Jankovic <juggealeks@gmail.com>
2015-08-28 10:35:48 +0200

    Devices without logged in user will follow public room

--- private/scripts/feed.js
+++ private/scripts/feed.js
@@ -3740,6 +3740,7 @@ function startBoot() {
       socketId : socket.id,
       user : 'NO_USER_LOGGED_IN'
     });
+    platformCmds.queueCommand(validCmds.follow.func, ['public']);
   }
 
   socket.emit('updateId', {
