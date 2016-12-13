5b82491b801146d4b8f960e98c3e216d387c6c17
Aleksandar Jankovic <aleksonline@icloud.com>
2016-12-13 07:47:40 +0100

    no message

--- config/defaults/appConfig.js
+++ config/defaults/appConfig.js
@@ -124,15 +124,15 @@ console.log(__dirname);
  * @type {{sitePath:string, filePath:string}[]}
  */
 config.routes = config.routes || [
-  { sitePath: '/', filePath: '../../../../routes/index.js' },
-  { sitePath: '/api/authenticate', filePath: '../../../../routes/rest/authenticate.js' },
-  { sitePath: '/api/rooms', filePath: '../../../../routes/rest/rooms.js' },
-  { sitePath: '/api/histories', filePath: '../../../../routes/rest/histories.js' },
-  { sitePath: '/api/positions', filePath: '../../../../routes/rest/positions.js' },
-  { sitePath: '/api/archives', filePath: '../../../../routes/rest/archives.js' },
-  { sitePath: '/api/messages', filePath: '../../../../routes/rest/messages.js' },
-  { sitePath: '/api/users', filePath: '../../../../routes/rest/users.js' },
-  { sitePath: '*', filePath: '../../../../routes/error.js' },
+  { sitePath: '/', filePath: '../../routes/index.js' },
+  { sitePath: '/api/authenticate', filePath: '../../routes/rest/authenticate.js' },
+  { sitePath: '/api/rooms', filePath: '../../routes/rest/rooms.js' },
+  { sitePath: '/api/histories', filePath: '../../routes/rest/histories.js' },
+  { sitePath: '/api/positions', filePath: '../../routes/rest/positions.js' },
+  { sitePath: '/api/archives', filePath: '../../routes/rest/archives.js' },
+  { sitePath: '/api/messages', filePath: '../../routes/rest/messages.js' },
+  { sitePath: '/api/users', filePath: '../../routes/rest/users.js' },
+  { sitePath: '*', filePath: '../../routes/error.js' },
 ];
 
 //
