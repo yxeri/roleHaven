81be4e2b2e2d4da0f8879f91d4a20ed59b6afff1
Aleksandar Jankovic <aleksonline@icloud.com>
2018-09-26 16:11:54 +0200

    New js

--- routes/index.js
+++ routes/index.js
@@ -59,7 +59,7 @@ function handle(io) {
       title: appConfig.title,
       gMapsKey: appConfig.gMapsKey,
       socketPath: appConfig.socketPath,
-      mainJs: 'scripts/main.js',
+      mainJs: 'scripts/main.js?new=1',
       mainCss: !isNaN(req.query.style) ? `styles/${req.query.style}.css` : 'styles/main.css',
       dyslexic: req.query.dyslexic,
     });
