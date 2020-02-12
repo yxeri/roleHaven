5766434b19d562ba1abc8ed131f69a2bb35b337a
Carmilla Mina Jankovic <carmillam@icloud.com>
2020-02-12 17:47:30 +0100

    Custom dbConfig

---
+++ config/dbConfig.js
@@ -0,0 +1,23 @@
+const config = {};
+
+config.teams = {};
+
+config.teams.detectives = {
+  objectId: '333333333333333333333330',
+  teamName: 'Detectives',
+  shortName: 'DETC',
+  isVerified: true,
+  auto: true,
+  ownerId: '222222222222222222222220',
+};
+
+config.teams.androids = {
+  objectId: '333333333333333333333331',
+  teamName: 'Free Androids',
+  shortName: 'FREE',
+  isVerified: true,
+  auto: true,
+  ownerId: '222222222222222222222220',
+};
+
+module.exports = config;
